760775235cf24105b491a9b5dd47fab1033122af
ichavera <Ivan.Chavera@itron.com>
2019-12-06 08:18:06 -0600

    Change nav bar to look for url pattern names

--- ads_site/ads_app/urls.py
+++ ads_site/ads_app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='ADS Dash Home'),
-    path('home', views.home, name='ADS Dash Home'),
-    path('test_plan', views.test_plan, name='ADS Dash Test Plan'),
-    path('update', views.update, name='Update Dashboard'),
+    url(r'^$', views.home, name='ADS Dash Home'),
+    url(r'^home', views.home, name='ADS Create Full Dash'),
+    url(r'^test_plan', views.test_plan, name='ADS Dash Test Plan'),
+    url(r'^update', views.update, name='Update Dashboard'),
 ]
